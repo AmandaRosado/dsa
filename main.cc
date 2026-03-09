51a2fe3176a9e4b0dae26335df12351e90169d49
Sona Tau Estrada Rivera <sona@stau.space>
2026-03-08 20:29:51 -0400

    Updated student copy.

--- main.cc
+++ main.cc
@@ -3,40 +3,11 @@ using std::cout;
 using std::endl;
 using std::string;
 
-template <typename T>
-struct Node {
-	T data;
-	Node<T>* next;
-};
-
-template <typename T>
-using List = Node<T>*;
-
 /// Empty list is null
-template <typename T>
-List<T> init() {
-	return nullptr;
-}
-
-template <typename T>
-void deinit(List<T>& list) {
-	if (list != nullptr) {
-		deinit(list->next);
-		delete list;
-		list = nullptr;
-	}
-}
 
 /// Requirements:
 /// input: Φ [a, b, c, ...]
 /// output: [Φ, a, b, c, ...]
-template <typename T>
-List<T> prepend(T elem, List<T> list) {
-	Node<T>* n = new Node<T>();
-	n->data = elem;
-	n->next = list;
-	return n;
-}
 
 /// Requirements:
 /// input: [a, Φ, c, ...] Φ
@@ -44,16 +15,6 @@ List<T> prepend(T elem, List<T> list) {
 ///
 /// input: [a, b, c, ...] Φ
 /// output: false
-template <typename T>
-bool contains(List<T> list, T elem) {
-	if (list == nullptr)
-		return false;
-
-	if (list->data == elem)
-		return true;
-
-	return contains(list->next, elem);
-}
 
 /// Requirements:
 /// input: [a, b, c, ...] 2
@@ -61,16 +22,6 @@ bool contains(List<T> list, T elem) {
 ///
 /// input: [a, b, c, ...] 0
 /// output: a
-template <typename T>
-T* index(List<T> list, unsigned int i) {
-	if (list == nullptr)
-		return nullptr;
-
-	if (i == 0)
-		return &list->data;
-
-	return index(list->next, i - 1);
-}
 
 /// Requirements:
 /// input: [a, b, c] 0
@@ -81,33 +32,11 @@ T* index(List<T> list, unsigned int i) {
 ///
 /// input: [a, b, c] 10000
 /// output: [a, b, c]
-template <typename T>
-List<T> remove_at(List<T> list, unsigned int offset) {
-	if (list == nullptr)
-		return nullptr;
-
-	if (offset == 0) {
-		List<T> l = list->next;
-		delete list;
-		return l;
-	}
-
-	list->next = remove_at(list->next, offset - 1);
-	return list;
-}
 
 // Helper function for printing the entire list
-template <typename T>
-void print(List<T> l) {
-	cout << "[";
-	for (string s = ""; l != nullptr; l = l->next) {
-		cout << s << l->data;
-		s = ", ";
-	}
-	cout << "]\n";
-}
 
 int main() {
+	/*
 	List<int> list = init<int>();
 
 	// build list: [1 .. 13]
@@ -158,4 +87,5 @@ int main() {
 	print(list);
 
 	deinit(list);
+	*/
 }
